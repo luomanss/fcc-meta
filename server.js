6f6911f431d7e9c1147949a05860c1b4902a7dbd
luomanss <sami.luomansuu@gmail.com>
2017-04-07 10:23:56 +0300

    fixed port issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const upload = multer();
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
